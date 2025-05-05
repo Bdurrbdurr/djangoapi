ff6f8f714d9d1a0f09561d398ecddf85f19ea631
Bdurrbdurr <venus3819@gmail.com>
2025-05-05 21:20:38 +0530

    Updated settings.py to host project

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6_5g(lq^2u6r-l@y74s*)5_5g2pu7v1+=qdtwd_h7!nt$8e5v*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
